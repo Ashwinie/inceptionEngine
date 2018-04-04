5e2116c18640441da0258c49acbec6371fc691c8
Shruthi Bhatt <Shruthi.SatheendranathBhatt@siriuscom.com>
2018-04-04 12:27:53 +0530

    Folder structure changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname + '/public/screen/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
