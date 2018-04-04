2afef99fa62f2e9a61eb7ae35323791b52d52772
Shruthi Bhatt <Shruthi.SatheendranathBhatt@siriuscom.com>
2018-04-04 12:18:14 +0530

    360/panaroma Image

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+// // Create the scene
+// var scene = document.createElement('a-scene');
+//
+// // Create the sky
+// var sky = document.createElement('a-sky');
+// sky.src = '3d-image.jpg';
+// sky.setAttribute('rotation', { x: 0, y: -130, y: 0 });
+//
+// // Inject into page
+// scene.appendChild(sky);
+// document.body.appendChild(scene);
