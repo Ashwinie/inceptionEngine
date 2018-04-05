3b2ac94b1ac0ab20e2da68e5f2fa8c5dfb71f3cd
Shruthi Bhatt <Shruthi.SatheendranathBhatt@siriuscom.com>
2018-04-05 23:34:38 +0530

    TTS changes

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,21 @@
 import witService from '../server/sevices/witService';
-var express = require('express'); 
+var express = require('express');
 var router = express.Router();
 import path from 'path';
 
 
 
+
 //Get Response from Witai
-router.get('/getResponse',function(req,res){
-  
-  if(req.query.sentence!=''){
+router.get('/getResponse', function(req, res) {
+
+  if (req.query.sentence != '') {
     console.log("Passing on the sentence to witservice" + JSON.stringify(req.query.sentence));
     witService.queryWit(req.query.sentence);
-  }else{
+  } else {
     console.log('Nothing to process');
   }
-  
   res.send(200);
 });
+
 module.exports = router;
